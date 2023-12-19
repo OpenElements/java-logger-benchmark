89feea6e3e3c19a970e1cff448f5817ec4be4a30
Hendrik Ebbers <hendrik.ebbers@web.de>
2023-12-19 15:13:15 +0100

    BenchScape Plugin used

--- logger-api/src/main/java/com/openelements/logger/api/BenchmarkConstants.java
+++ logger-api/src/main/java/com/openelements/logger/api/BenchmarkConstants.java
@@ -2,15 +2,15 @@ package com.openelements.logger.api;
 
 public class BenchmarkConstants {
 
-    public final static int WARMUP_ITERATIONS = 2;
+    public final static int WARMUP_ITERATIONS = 1;
 
-    public final static int WARMUP_TIME_IN_SECONDS_PER_ITERATION = 2;
+    public final static int WARMUP_TIME_IN_SECONDS_PER_ITERATION = 1;
 
-    public final static int MEASUREMENT_ITERATIONS = 2;
+    public final static int MEASUREMENT_ITERATIONS = 1;
 
-    public final static int MEASUREMENT_TIME_IN_SECONDS_PER_ITERATION = 2;
+    public final static int MEASUREMENT_TIME_IN_SECONDS_PER_ITERATION = 1;
 
-    public final static int PARALLEL_THREAD_COUNT = 4;
+    public final static int PARALLEL_THREAD_COUNT = 1;
 
     public final static int FORK_COUNT = 1;
 
